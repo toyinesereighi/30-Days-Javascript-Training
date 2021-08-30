38c0f1ac7f8405a0dc41234953449ced02dc6c51
toyinesereighi <87209402+toyinesereighi@users.noreply.github.com>
2021-08-30 18:49:31 +0100

    Add files via upload

---
+++ task 2/index.js
@@ -0,0 +1,10 @@
+let myAge = 21
+
+let userAge = prompt('How old are you?')
+if (userAge <= 12){
+    alert(`You're a child`)
+} else if(userAge <= 19){
+    alert(`You're a teenager`)
+} else
+    alert(`You're an adult`)
+
